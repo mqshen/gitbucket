d823267eca39020b96f77c265dd963c4bb9bc8d2
mqshen <mqshen@126.com>
2016-03-05 11:51:07 +0800

    add

--- src/main/scala/servlet/SecurityPolicyFilter.scala
+++ src/main/scala/servlet/SecurityPolicyFilter.scala
@@ -3,9 +3,6 @@ package servlet
 import javax.servlet._
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 
-import org.slf4j.LoggerFactory
-import util.Keys
-
 /**
  * Created by goldratio on 3/5/16.
  */
@@ -17,6 +14,8 @@ class SecurityPolicyFilter extends Filter {
   def doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain): Unit = {
     res match {
       case response: HttpServletResponse  =>
+        println("=====================")
+        println("start security-policy")
         response.setHeader("Content-Security-Policy", "default-src *; base-uri 'self'; block-all-mixed-content; child-src 'self' 101.251.195.186:18090; connect-src 'self' 101.251.195.186:18090; font-src assets-cdn.github.com; form-action 'self' 101.251.195.186:18090; frame-src 'self' 101.251.195.186:18090; img-src 'self' mqshen.qiniudn.com; media-src 'none'; lugin-types application/x-shockwave-flash; script-src 'self' 101.251.195.186:18090; style-src 'self' 'unsafe-inline'")
       case _ =>
     }

98fe1679b9f0343ed453b7bce5c6c3f3d7bce379
mqshen <mqshen@126.com>
2016-03-05 11:55:44 +0800

    add

--- src/main/scala/servlet/SecurityPolicyFilter.scala
+++ src/main/scala/servlet/SecurityPolicyFilter.scala
@@ -16,7 +16,7 @@ class SecurityPolicyFilter extends Filter {
       case response: HttpServletResponse  =>
         println("=====================")
         println("start security-policy")
-        response.setHeader("Content-Security-Policy", "default-src *; base-uri 'self'; block-all-mixed-content; child-src 'self' 101.251.195.186:18090; connect-src 'self' 101.251.195.186:18090; font-src assets-cdn.github.com; form-action 'self' 101.251.195.186:18090; frame-src 'self' 101.251.195.186:18090; img-src 'self' mqshen.qiniudn.com; media-src 'none'; lugin-types application/x-shockwave-flash; script-src 'self' 101.251.195.186:18090; style-src 'self' 'unsafe-inline'")
+        response.setHeader("Access-Control-Allow-Origin", "*")
       case _ =>
     }
     chain.doFilter(req, res)
